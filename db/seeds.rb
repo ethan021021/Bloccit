8bb4fcf45a9b361244f71d2d8e3167ef00007669
Ethan Thomas <ethan021@me.com>
2017-02-12 16:31:28 -0800

    assesment 17: complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,21 @@ require 'random_data'
   Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)
 end
 
+unique_post = '';
+
+1.times do
+  unique_post = Post.find_or_create_by!(title: "Custom Title", body: "Custom Body")
+end
+
 posts = Post.all
 
 100.times do
   Comment.create!(post: posts.sample,
   body: RandomData.random_paragraph)
+end
+
+1.times do
+  Comment.find_or_create_by!(post: unique_post, body: "Custom Comment Body")
 end
 
 puts "Seed finished"
